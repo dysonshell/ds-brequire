87251ba4b567f4732589efb59271bc53c504f1a7
Anson Yu Wang <opensource@undozen.com>
2015-04-29 10:35:59 +0800

    add test cases

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 'use strict';
 require('@ds/common');
+var fs = require('fs');
+var path = require('path');
 var Browserify = require('browserify');
 var co = require('co');
 var xtend = require('xtend');
 
 var _createDeps = Browserify.prototype._createDeps;
-Browserify.prototype._createDeps = function (opts) {
+Browserify.prototype._createDeps = function(opts) {
     var _mdeps = _createDeps.call(this, opts);
     var mdeps = _mdeps.constructor;
     var options = _mdeps.options;
     var oresolve = options.resolve;
+
     function coresolve(id, parent) {
-        return new Promise(function (r) {
-            oresolve(id, parent, function (err, file, pkg) {
+        return new Promise(function(r) {
+            oresolve(id, parent, function(err, file, pkg) {
                 r({
                     err: err,
                     file: file,
@@ -21,15 +24,17 @@ Browserify.prototype._createDeps = function (opts) {
             });
         });
     }
+
     function exists(filePath) {
-        return new Promise(function (resolve) {
+        return new Promise(function(resolve) {
             fs.exists(filePath, resolve);
         });
     }
-    options.resolve = co.wrap(function *(id, parent, cb) {
+    options.resolve = co.wrap(function * (id, parent, cb) {
         var originalParentFilename;
         if (parent.filename.indexOf('/ccc/') === -1 &&
-            parent.filename.indexOf('/node_modules/@ccc/') === -1) {
+            parent.filename.indexOf('/node_modules/@ccc/') === -1 &&
+            id.indexOf('ccc/') !== 0 ) {
             return oresolve(id, parent, cb);
         }
         var results = [];
@@ -42,16 +47,16 @@ Browserify.prototype._createDeps = function (opts) {
                 results.push(yield coresolve(id.replace(/^ccc\//, '@ccc/'), parent));
             }
         } else {
-            // 从 `/ccc/` 里面 require 的，先直接找，再以 @ccc 下面为 fallback
+            // 从 `/ccc/` 里面 require 或者普通路径 require ccc/ 下面的，先直接找，再以 @ccc 下面为 fallback
             results.push(yield coresolve(id, parent));
             var repParFile = parent.filename.replace('/ccc/', '/node_modules/@ccc/');
-            if (results.slice(-1)[0].err && (yield exists(repParFile)) && id.indexOf('ccc/') !== 0) {
+            if (results.slice(-1)[0].err && (id.indexOf('ccc/') === 0 || (yield exists(repParFile)))) {
                 results.push(yield coresolve(id.replace(/^ccc\//, '@ccc/'), xtend(parent, {
                     basedir: path.dirname(repParFile)
                 })));
             }
         }
-        var result = results.filter(function (r) {
+        var result = results.filter(function(r) {
             return !r.err;
         })[0];
         if (!result) {
--- test/cases/brequire.js
+++ test/cases/brequire.js
@@ -1,1 +1,27 @@
 'use strict';
+var path = require('path');
+var vm = require('vm');
+var tape = require('tape');
+var browserify = require('browserify');
+require('../../');
+
+tape(function(test) {
+    test.plan(5);
+    browserify('./hello.js', {
+        basedir: path.resolve(__dirname, '..'),
+        fullPaths: true
+    }).bundle(function(err, source) {
+        console.log(arguments);
+        console.log(source.toString('utf-8'));
+        test.ok(!err);
+        var sandbox = {};
+        sandbox.global = sandbox.GLOBAL = sandbox;
+        vm.runInNewContext(source.toString('utf-8'), sandbox);
+        console.log('sandbox', sandbox);
+        test.equal(sandbox.a, 'ma');
+        test.equal(sandbox.b, 'b');
+        test.equal(sandbox.c, 'mc');
+        test.equal(sandbox.d, 'md');
+        console.log(err);
+    });
+});
