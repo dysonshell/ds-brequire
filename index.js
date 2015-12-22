f5828b8bb8730f3feba353f1a2533b165c6ad0b6
Anson Yu Wang <opensource@undozen.com>
2015-12-23 00:25:20 +0800

    drop co to rethrow module not found error

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ var assert = require('assert');
 var config = require('config');
 assert(config.dsAppRoot);
 var Browserify = require('browserify');
-var co = require('co');
 var xtend = require('xtend');
 
 // config
@@ -20,67 +19,57 @@ Browserify.prototype._createDeps = function(opts) {
     var options = _mdeps.options;
     var oresolve = options.resolve;
 
-    function coresolve(id, parent) {
-        return new Promise(function(r) {
-            oresolve(id, parent, function(err, file, pkg) {
-                r({
-                    err: err,
-                    file: file,
-                    pkg: pkg,
-                });
-            });
-        });
-    }
-
     function exists(filePath) {
         return new Promise(function(resolve) {
             fs.exists(filePath, resolve);
         });
     }
-    options.resolve = co.wrap(function * (id, parent, cb) {
+    options.resolve = function (id, parent, ocb) {
         var originalParentFilename;
+        var cb = function (err, file, pkg) {
+            if (err && typeof err.message === 'string') {
+                err.message += " (file '" + parent.filename + "')";
+            }
+            ocb(err, file, pkg);
+        }
         if (parent.filename.indexOf('/'+DSC) === -1 &&
             parent.filename.indexOf('/node_modules/@'+DSC) === -1 &&
             id.indexOf(DSC) !== 0 ) {
             return oresolve(id, parent, cb);
         }
         var results = [];
+        id = id.replace(new RegExp('^'+DSC), DSC);
         if (parent.filename.indexOf('/node_modules/@'+DSC) > -1) {
             // 从 @dsc 里面 require 的，先尝试在 /dsc/ 里面找对应的
-            results.push(yield coresolve(id, xtend(parent, {
+            oresolve(id, xtend(parent, {
                 basedir: path.dirname(parent.filename.replace('/node_modules/@'+DSC, '/'+DSC))
-            }, (typeof APP_ROOT === 'string' && id.indexOf(DSC) === 0) ? {
+            }, (id.indexOf(DSC) === 0) ? {
                 paths: [APP_ROOT].concat(parent.paths),
-            } : {})));
-            if (results.slice(-1)[0].err) {
-                results.push(yield coresolve(id.replace(new RegExp('^'+DSC), '@'+DSC), parent));
-            }
+            } : {}), function (err, file, pkg) {
+                if (err) {
+                    oresolve(id.replace(new RegExp('^'+DSC), '@'+DSC), parent, cb);
+                } else {
+                    cb(err, file, pkg);
+                }
+            });
         } else {
             // 从 `/dsc/` 里面 require 或者普通路径 require dsc/ 下面的，先直接找，再以 @dsc 下面为 fallback
-            results.push(yield coresolve(id, parent));
-            var repParFile = parent.filename.replace('/'+DSC, '/node_modules/@'+DSC);
-            if (results.slice(-1)[0].err) {
-                if (typeof APP_ROOT === 'string' && id.indexOf(DSC) === 0) {
-                    results.push(yield coresolve(id, xtend(parent, {
-                        paths: [APP_ROOT].concat(parent.paths),
-                        basedir: APP_ROOT
-                    })));
-                }
-                if (results.slice(-1)[0].err) {
-                    results.push(yield coresolve(id.replace(new RegExp('^'+DSC), '@'+DSC), xtend(parent, {
+            var xparent = xtend(parent, {
+                paths: [APP_ROOT].concat(parent.paths),
+                basedir: APP_ROOT
+            });
+            oresolve(id, xparent, function (err, file, pkg) {
+                var repParFile = parent.filename.replace('/'+DSC, '/node_modules/@'+DSC);
+                if (err) {
+                    oresolve(id.replace(new RegExp('^'+DSC), '@'+DSC), xtend(parent, {
                         filename: repParFile,
                         basedir: path.dirname(repParFile)
-                    })));
+                    }), cb);
+                } else {
+                    cb(err, file, pkg);
                 }
-            }
+            });
         }
-        var result = results.filter(function(r) {
-            return !r.err;
-        })[0];
-        if (!result) {
-            return cb(results[0].err);
-        }
-        cb(null, result.file, result.pkg);
-    });
+    };
     return mdeps(options);
 }
